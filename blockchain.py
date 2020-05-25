3bd499078efb8b26f735569b6c15a968d29eabbd
collozy <ozlocollins@gmail.com>
2020-05-25 12:50:26 +0300

    Added mining function with stringified hash values of blocks

--- blockchain.py
+++ blockchain.py
@@ -1,6 +1,13 @@
 # Initializing the blockchain list
 blockchain = []
+genesis_block = {'previous_hash': '',
+             'index': 0,
+             'transactions': []}
+blockchain.append(genesis_block)
 continue_displaying_program = True
+open_transactions = []
+owner = 'Oslo'
+
 
 def get_last_blockchain_item():
         """ Returns the last value of the blockchain """
@@ -12,7 +19,10 @@ def get_last_blockchain_item():
 
 
 def get_user_input():
-    return float(input("Please enter the amount: "))
+    """ Returns a tuple containing amount and recipient of a transactions: (recipient, amount) """
+    recipient_input = input("Enter the recipient of the transaction: ")
+    amount_input = float(input("Please enter the transaction amount: "))
+    return recipient_input, amount_input
 
 
 def get_user_option():
@@ -35,18 +45,29 @@ def verify_chain():
     return is_valid
 
 
-def add_item(previous_transaction):
+def add_item(recipient, sender=owner, amount=1.0):
     """ Append a new value to the blockchain together with the previous blockchain value.
 
-    Argument:
-        :previous_transaction: The last blockchain transaction (default [1]).
+    Arguments:
+        :sender: The sender of the coins.
+        :recipient: The recipient of the coins.
+        :amount: The amount of coins sent with the transaction (default=1.0)
     """
+    transaction = {'sender': sender, 'recipient': recipient, 'amount': amount}
+    open_transactions.append(transaction)
 
-    if previous_transaction == None:
-        previous_transaction = [1]
 
-    blockchain_transaction_amount = get_user_input()
-    blockchain.append([previous_transaction, blockchain_transaction_amount])
+def mine_new_block():
+    last_block = blockchain[-1]
+    hashed_block = ''
+    for key in last_block:
+        value = last_block[key]
+        hashed_block += str(value)
+    block = {'previous_hash': 'XYZ',
+             'index': len(blockchain),
+             'transactions': open_transactions}
+    blockchain.append(block)
+    print(hashed_block)
 
 
 def display_blockchain_blocks():
@@ -58,26 +79,33 @@ def display_blockchain_blocks():
 while continue_displaying_program:
     print("Please choose an option: ")
     print("1: Add a new transaction value")
-    print("2: Output the blockchain blocks")
-    print("3: Manipulate the chain")
-    print("4: Quit the program")
+    print("2: Mine new block")
+    print("3: Output the blockchain blocks")
+    print("4: Manipulate the chain")
+    print("5: Quit the program")
     user_input = get_user_option()
 
     if user_input == 1:
-        add_item(get_last_blockchain_item())
+        data = get_user_input()
+        recipient, amount = data
+        # Add transaction amount to the blockchain
+        add_item(recipient, amount=amount)
+        print(open_transactions)
     elif user_input == 2:
-        display_blockchain_blocks()
+        mine_new_block()
     elif user_input == 3:
+        display_blockchain_blocks()
+    elif user_input == 4:
         if len(blockchain) >= 1:
             blockchain[0] = [2]
-    elif user_input == 4:
+    elif user_input == 5:
         continue_displaying_program = False
     else:
         print("Sorry, you entered an invalid value.")
 
-    if not verify_chain():
-        print("Invalid blockchain")
-        break
+    # if not verify_chain():
+    #     print("Invalid blockchain")
+    #     break
 
 
 
